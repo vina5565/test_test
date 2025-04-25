8c81b41de08b139903289bb3a9fb8e2baf688290
vina5565 <azazgadee@naver.com>
2025-04-25 17:48:08 +0900

    Python_test

--- hw6.py
+++ hw6.py
@@ -1,8 +1,17 @@
+def input_number(prompt) :
+    n = 0
+    while n <= 0 :
+        n = int(input(prompt))
+    
+    return n
 
-for j in range(2) :# j *= 0, 1 
-    for k in range(9) : # k = 0 ~ 8
-        for i in range(4) : # i = 0 ~ 3
-            dan = 9 - ( i + j * 4)
-            print(f"{dan} x {9-k} = {dan*(9-k):2}", end = '   ') # f"{식} = {값:2}"	출력 값을 두 자리 정렬로 예쁘게 맞춤
-        print()
-    print()
+def display_multiplication(n) :
+
+    i = 1
+    while i <= 9 :
+        print(f"{n} x {i} = {n * i:2d}")
+        i += 1
+
+
+n = input_number("양의 정수를 입력하시오 : ")
+display_multiplication(n)   
